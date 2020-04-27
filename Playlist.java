33e6921653c82079c97309b0dfaeed73cf4ad26c
Tamara Lawlor <55025022+TTLawlor@users.noreply.github.com>
2020-04-27 19:31:45 +0100

    Update Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -49,25 +49,6 @@ public class Playlist{
         System.out.println(playlist);
     }
     
-    public void playOnly(String artist){
-        for(int i = 0; i < playlist.size(); i++){
-            artist = artist.toLowerCase();
-            String playlistArtist = playlist.get(i).get(1).toLowerCase();
-            if (playlistArtist.contains(artist)){
-                System.out.println(playlist.get(i));
-            }
-        }
-    }
-    
-    public void playOnly(int year){
-        for(int i = 0; i < playlist.size(); i++){
-            int playlistYear = Integer.parseInt(playlist.get(i).get(2));
-            if (year == playlistYear){
-                System.out.println(playlist.get(i));
-            }
-        }
-    }
-    
     public void play(boolean random){
         if(!random){
             for(int i = 0; i < playlist.size(); i++){
